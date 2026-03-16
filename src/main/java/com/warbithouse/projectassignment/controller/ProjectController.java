fc9fb9d8a903812175a82ad6c497053daf7147a9
Junior <eecfredes@gmail.com>
2026-03-15 21:15:16 -0300

    :rocket:Get Project method

--- src/main/java/com/warbithouse/projectassignment/controller/ProjectController.java
+++ src/main/java/com/warbithouse/projectassignment/controller/ProjectController.java
@@ -1,11 +1,16 @@
 package com.warbithouse.projectassignment.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.warbithouse.projectassignment.model.Project;
 import com.warbithouse.projectassignment.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @CrossOrigin(origins = "${SPRING_ORIGINS:*}")
@@ -13,5 +18,10 @@ import com.warbithouse.projectassignment.repository.ProjectRepository;
 public class ProjectController {
     @Autowired
     private ProjectRepository projectRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Project>> getProject() {
+        return ResponseEntity.ok(projectRepository.findAll());
+    }
 
 }
