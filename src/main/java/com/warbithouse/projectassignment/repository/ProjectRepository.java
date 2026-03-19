f3fc4b2985bcffa9dcdb3078787453fda441a812
Junior <eecfredes@gmail.com>
2026-03-18 22:42:49 -0300

    :rocket:Project Repository Update

--- src/main/java/com/warbithouse/projectassignment/repository/ProjectRepository.java
+++ src/main/java/com/warbithouse/projectassignment/repository/ProjectRepository.java
@@ -1,11 +1,19 @@
 package com.warbithouse.projectassignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.warbithouse.projectassignment.model.Project;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM STUDENT_PROJECT WHERE PROJECT_ID=?1", nativeQuery = true)
+    public void deleteFromStudentProjectByProjectId(Integer project_id);
 
 }
