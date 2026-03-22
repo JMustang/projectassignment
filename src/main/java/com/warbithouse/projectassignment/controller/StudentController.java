a5e86ce5d1e5b25355e80c975dccdcbe572b18c0
Junior <eecfredes@gmail.com>
2026-03-21 22:40:49 -0300

    :rocket:Get all Student Method

--- src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
+++ src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
@@ -1,10 +1,15 @@
 package com.warbithouse.projectassignment.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.warbithouse.projectassignment.model.Student;
 import com.warbithouse.projectassignment.repository.ProjectRepository;
 import com.warbithouse.projectassignment.repository.StudentRepository;
 
@@ -17,4 +22,9 @@ public class StudentController {
     private ProjectRepository projectRepository;
     @Autowired
     private StudentRepository studentRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Student>> GetAllStudents() {
+        return ResponseEntity.ok(studentRepository.findAll());
+    }
 }
