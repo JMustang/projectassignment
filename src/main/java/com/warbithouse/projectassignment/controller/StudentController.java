03e5c35a1378f56e5dbb23940d9d6cf4d417257a
Junior <eecfredes@gmail.com>
2026-03-23 21:44:12 -0300

    :rocket:Retrieve Student and Project

--- src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
+++ src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.warbithouse.projectassignment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.warbithouse.projectassignment.model.Project;
 import com.warbithouse.projectassignment.model.Student;
 import com.warbithouse.projectassignment.repository.ProjectRepository;
 import com.warbithouse.projectassignment.repository.StudentRepository;
@@ -30,6 +32,16 @@ public class StudentController {
     @GetMapping("/")
     public ResponseEntity<List<Student>> getAllStudents() {
         return ResponseEntity.ok(studentRepository.findAll());
+    }
+
+    public Student retrieveStudentById(int id) {
+        return studentRepository.findById(id).orElseThrow(
+                () -> new ExpressionException("❌ Student with id " + id + " is not found!"));
+    }
+
+    public Project retrieveProjectById(int id) {
+        return projectRepository.findById(id).orElseThrow(
+                () -> new ExpressionException("❌ Project with id " + id + " is not found!"));
     }
 
     @GetMapping("/{id}")
