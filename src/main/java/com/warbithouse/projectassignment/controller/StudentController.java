37847bc6bd8288911d241c590d5fda421d241ee2
Junior <eecfredes@gmail.com>
2026-03-25 22:41:41 -0300

    :rocket:Student delete method

--- src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
+++ src/main/java/com/warbithouse/projectassignment/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.expression.ExpressionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,5 +63,12 @@ public class StudentController {
         student.setName(studentDetails.getName());
         student.setAverage(studentDetails.getAverage());
         return ResponseEntity.ok(studentRepository.save(student));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Student student = retrieveStudentById(id);
+        studentRepository.delete(student);
+        return ResponseEntity.ok("Student deleted Successfully!");
     }
 }
